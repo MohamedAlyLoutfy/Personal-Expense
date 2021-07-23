c9f52ea52714a14aec112590ddf4656b82908564
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-07-23 22:21:09 +0200

    adding transaction item widget

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -4,12 +4,9 @@ class ChartBar extends StatelessWidget {
 
   final String label;
   final double spendingAmount;
-
-
-
   final double spendingPctOfTotal;
 
-  ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
+  const ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
 
   @override
   Widget build(BuildContext context) {
