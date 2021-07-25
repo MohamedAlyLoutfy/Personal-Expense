161c12d1be3eab2e4b943d88d351945f9ccc7fef
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-07-25 12:49:21 +0200

    Using builder methods

--- lib/main.dart
+++ lib/main.dart
@@ -123,11 +123,72 @@ void _deleteTransaction(String id){
         return tx.id==id;
       });
     });
+  }
+
+  List<Widget> _buildLandscapeContent(
+      MediaQueryData mediaQuery
+   ,AppBar appBar,
+   Widget txListWidget
+
+
+  ) {
+    return [Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Show Chart',
+                style: Theme.of(context).textTheme.title,
+                
+                ),
+                Switch.adaptive
+                (activeColor: Theme.of(context).accentColor,
+                  value: _showChart, 
+                onChanged: (val){
+                  setState(() {
+                    _showChart=val;
+                    
+                  });
+
+
+                }, ),
+
+
+              ],
+
+
+            ),
+                  _showChart
+         ? Container(
+            height:(mediaQuery.size.height -
+            appBar.preferredSize.height-
+            MediaQuery.of(context).padding.top
+            )*0.7 ,
+            child: Chart(_recentTransactions),
+            )
+            : txListWidget
+            ];
 
 
 
   }
+   List<Widget> _buildPortraitContent(
+     MediaQueryData mediaQuery
+   ,AppBar appBar,
+   Widget txListWidget
+   
+   ) {
+     return [Container(
+            height:(mediaQuery.size.height -
+            appBar.preferredSize.height-
+            mediaQuery.padding.top
+            )*0.3 ,
+            child: Chart(_recentTransactions),
+            ),
+            txListWidget];
+            
 
+
+
+   }
   @override
   Widget build(BuildContext context) {
     final mediaQuery=MediaQuery.of(context);
@@ -177,48 +238,19 @@ void _deleteTransaction(String id){
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            if(isLandscape) Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('Show Chart',
-                style: Theme.of(context).textTheme.title,
-                
-                ),
-                Switch.adaptive
-                (activeColor: Theme.of(context).accentColor,
-                  value: _showChart, 
-                onChanged: (val){
-                  setState(() {
-                    _showChart=val;
-                    
-                  });
+            if(isLandscape)  ..._buildLandscapeContent(  mediaQuery, 
+              appBar,
+              txListWidget),
 
-
-                }, ),
-
-
-              ],
-
-
-            ),
-            if(!isLandscape)
-            Container(
-            height:(mediaQuery.size.height -
-            appBar.preferredSize.height-
-            mediaQuery.padding.top
-            )*0.3 ,
-            child: Chart(_recentTransactions),
-            ),
-            if(!isLandscape) txListWidget,
-            if(isLandscape)    _showChart
-         ? Container(
-            height:(mediaQuery.size.height -
-            appBar.preferredSize.height-
-            MediaQuery.of(context).padding.top
-            )*0.7 ,
-            child: Chart(_recentTransactions),
-            )
-            : txListWidget
+             
+            if(!isLandscape) ..._buildPortraitContent(
+              mediaQuery, 
+              appBar,
+              txListWidget),
+            
+   
+            
+         
          
           ],
         ),
